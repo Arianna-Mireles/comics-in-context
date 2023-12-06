3fdccb2bff2e744eaff499cf15e16ff1bdd5a83d
Arianna Mireles <arianna.mireles02@gmail.com>
2023-12-06 00:32:29 -0600

    CollectorButton complete

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import StylesContext from "./components/StylesContext";
 
 function App() {
   const stylin = {
-  background: "#1ECD97",
-    display: "inline-block", 
-    width: 120,
-    height: 55,
-    fontSize: 18,
-    letterSpacing: 1,
-    border: "2px solid #1ECD97",
-    borderRadius: 15
-  };
+    background: "#1ECD97",
+      display: "inline-block", 
+      width: 120,
+      height: 55,
+      fontSize: 18,
+      letterSpacing: 1,
+      border: "2px solid #1ECD97",
+      borderRadius: 15
+    };
   
   return (
     <main>
