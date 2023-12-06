6fc2dd3acb73ea5af4119d8bee779235b1b6eefd
Arianna Mireles <arianna.mireles02@gmail.com>
2023-12-06 00:51:28 -0600

    Add css and finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 
-import UserInterface from "./components/UserInterface"
+import UserInterface from "./components/UserInterface";
 import StylesContext from "./components/StylesContext";
 
 
 function App() {
   const stylin = {
     background: "#1ECD97",
-      display: "inline-block", 
-      width: 120,
-      height: 55,
-      fontSize: 18,
-      letterSpacing: 1,
-      border: "2px solid #1ECD97",
-      borderRadius: 15
-    };
+    display: "inline-block", 
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #1ECD97",
+    borderRadius: 15
+  };
   
   return (
     <main>
